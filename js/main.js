b430feef00c2e8388c82b66294a947e964f3daf6
faisan <faisan.m@cied.in>
2024-09-04 20:21:12 +0530

    Final s

--- js/main.js
+++ js/main.js
@@ -76,6 +76,34 @@
 
 
 
+    // vegetable carousel
+    $(".vegetable-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
 
 	// var loader = function() {
 	// 	setTimeout(function() { 
